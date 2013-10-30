eab6fc99f8d8d130943b231c919a3fbd3a6fd520
macsj200 <macsj200@gmail.com>
2013-10-30 11:20:40 -0600

    Add GameWindow class

---
+++ JavaPokemon/src/gui/GameWindow.java
@@ -0,0 +1,13 @@
+package gui;
+
+import javax.swing.JFrame;
+
+public class GameWindow extends JFrame {
+	public GameWindow(){
+		super("JavaPokemon");
+		//Set title of window
+		
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setVisible(true);
+	}
+}
