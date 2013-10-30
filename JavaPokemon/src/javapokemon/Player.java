23a54d9adce213c0558a0ac8a2e5501a1768b0f5
macsj200 <macsj200@gmail.com>
2013-10-30 11:15:04 -0600

    Add stagedPokemon to Player and stagePokemon() method (add fainted check)

--- JavaPokemon/src/javapokemon/Player.java
+++ JavaPokemon/src/javapokemon/Player.java
@@ -2,7 +2,21 @@ package javapokemon;
 
 import java.util.ArrayList;
 
+import exceptions.FaintedException;
+
 public class Player {
 	private ArrayList<Pokemon> party = null;
 	//ArrayList that contains party
+	
+	private Pokemon stagedPokemon = null;
+	//Currently staged Pokemon
+	
+	public void stagePokemon(Pokemon pokemon) throws FaintedException{
+		if(pokemon.isFainted()){
+			throw new FaintedException(pokemon);
+		}
+		else{
+			stagedPokemon = pokemon;
+		}
+	}
 }
