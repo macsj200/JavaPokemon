1bdeeeea6740bd108516b10c38d5c6aa701c03ca
macsj200 <macsj200@gmail.com>
2013-10-30 11:16:06 -0600

    Instantiate player in JavaPokemon

--- JavaPokemon/src/javapokemon/JavaPokemon.java
+++ JavaPokemon/src/javapokemon/JavaPokemon.java
@@ -1,10 +1,15 @@
 package javapokemon;
 
 public class JavaPokemon {
+	private Player player = null;
 
 	public static void main(String[] args) {
 		new JavaPokemon();
 		//Instantiate itself
+	}
+	
+	public JavaPokemon(){
+		player = new Player();
 	}
 
 }
