6c7764bf03c7989362517bb30b99b53f75ee9bbb
macsj200 <macsj200@gmail.com>
2013-11-01 11:58:42 -0600

    Add HUD

---
+++ JavaPokemon/src/gui/HUD.java
@@ -0,0 +1,11 @@
+package gui;
+
+import javax.swing.ImageIcon;
+import javax.swing.JPanel;
+
+public class HUD extends JPanel {
+	private ImageIcon sprite = null;
+	private JPanel healthBar = null;
+	
+	
+}
