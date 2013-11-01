15f1fb9f64859834bc46d4d8e8e9f806587b012c
macsj200 <macsj200@gmail.com>
2013-11-01 07:39:49 -0600

    Add setStats method

--- JavaPokemon/src/javapokemon/Pokemon.java
+++ JavaPokemon/src/javapokemon/Pokemon.java
@@ -1,10 +1,20 @@
 package javapokemon;
 
 public class Pokemon {
+	private String name = null;
+	
+	Pokemon(String name){
+		this.name = name;
+		setStats();
+	}
 
 	public boolean isFainted() {
 		// TODO Auto-generated method stub
 		return false;
+	}
+	
+	private void setStats(){
+		
 	}
 	
 }
