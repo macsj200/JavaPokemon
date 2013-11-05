36ad380fe44082768030d0c94efc5c57d2176e95
nsilverii15 <nick.silverii@yahoo.com>
2013-11-05 09:01:48 -0700

    Create moves Array Populate moves Array through database Call factory method

--- JavaPokemon/src/databases/MovesDatabase.java
+++ JavaPokemon/src/databases/MovesDatabase.java
@@ -12,13 +12,17 @@ public class MovesDatabase {
 	private BufferedReader reader = null;
 	private String line = null;
 	private String csvSplitBy = ",";
+	private MovesFactory factory = null;
 
 	public MovesDatabase(String dataFile){
 		this.dataFile = dataFile;
+		factory = new MovesFactory();
 	}
 
 	public Move[] getMoves(String pokemonName){
 		Move[] moves = new Move[4];
+		String[] splitLine = null;
+		
 		try {
 			reader = new BufferedReader(new FileReader(dataFile));
 		} catch (FileNotFoundException e) {
@@ -27,8 +31,15 @@ public class MovesDatabase {
 		
 		try {
 			while((line = reader.readLine()) != null){
-				if(line.split(csvSplitBy)[0].equals(pokemonName)){
-					//Do stuff
+				splitLine = line.split(csvSplitBy);
+				
+				if(splitLine[0].equals(pokemonName)){
+					moves[0] = factory.makeMove(splitLine[1]);
+					moves[1] = factory.makeMove(splitLine[2]);
+					moves[2] = factory.makeMove(splitLine[3]);
+					moves[3] = factory.makeMove(splitLine[4]);
+					
+					
 				}
 			}
 		} catch (IOException e) {
