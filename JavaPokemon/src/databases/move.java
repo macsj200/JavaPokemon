e28c7ca73ffa3bba6cc5d9f68f10c9d6a05616b9
jesmith14 <jesmith14@10.1.44.134>
2013-11-11 08:26:05 -0700

    Create Move constructors

---
+++ JavaPokemon/src/databases/move.java
@@ -0,0 +1,14 @@
+package databases;
+
+public class move {
+	private String type;
+	private int power;
+	private float accuracy;
+	
+	public move(String moveType, int movePower, float moveAccuracy){
+		type = moveType;
+		power = movePower;
+		accuracy = moveAccuracy;
+	}
+	
+}
