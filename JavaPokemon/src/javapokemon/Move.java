55b2202d3817dc6f04a55fa2132a4769c0de0ad1
nsilverii15 <nick.silverii@yahoo.com>
2013-11-13 07:51:46 -0700

    Make Move class mo betta

--- JavaPokemon/src/javapokemon/Move.java
+++ JavaPokemon/src/javapokemon/Move.java
@@ -1,5 +1,13 @@
 package javapokemon;
 
 public class Move {
+	private String type;
+	private int power;
+	private float accuracy;
 	
+	public Move(String moveType, int movePower, float moveAccuracy) {
+		type = moveType;
+		power = movePower;
+		accuracy = moveAccuracy;
+	}
 }
